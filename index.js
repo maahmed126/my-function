ca35fcd3612e0a0e779c6ea2a65f519c72039f2a
Md Ahmed <maahmed126@gmail.com>
2021-11-11 22:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const AWS = require("aws-sdk");
+const apigateway = new AWS.APIGateway();
+
 exports.handler = async (event) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify("Hello from Lambda and test"),
-    }
-    return response
-  }
-  
+
+    var params = {
+        apiKey: "6Unzbh7iib1zFi4aPACVT1AzbU3fTuIh3gM2zVFw",
+        includeValue: true
+    };
+
+    const apikey = await apigateway.getApiKey(params).promise();
+    console.log("apikey:", apikey);
+    return apikey;
+};
